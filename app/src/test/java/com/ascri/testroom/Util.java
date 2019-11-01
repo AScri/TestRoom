d3477b682980eafe2ec12c840bb70f6e34df969f
user <user@mail.com>
2019-11-01 18:28:45 +0200

    branch1 chabges

--- app/src/test/java/com/ascri/testroom/Util.java
+++ app/src/test/java/com/ascri/testroom/Util.java
@@ -20,6 +20,7 @@ public class Util {
             //++ee________
             //change user
             //branch1 changes
+            //branch1 changes
             //master
             //branch2
         }
